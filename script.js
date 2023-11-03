a434b2db0ae50094cb10c453b648b20e6d915aa6
Ian Gazcka Arechiga <iangazcka@gmail.com>
2023-11-02 18:35:37 -0600

    Primer avance

---
+++ script.js
@@ -0,0 +1,9 @@
+﻿var d;
+$(document).ready(function(){
+    var interval = setInterval(prueba,1000);
+})
+
+function prueba() {
+    d = new Date();
+    $('#item').html('La hora en este momento es: '+ d.toString());
+}
